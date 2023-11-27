1c7f14b264bc93fac53ddd9428b0c2edd303b874
Eugene Rabenko <qwer09846@gmail.com>
2023-11-27 12:03:32 +0200

    create index page with task list

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from todo_app.models import Task
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    queryset = Task.objects.prefetch_related("tags")
+
+    def get_queryset(self):
+        return Task.objects.order_by('is_done', 'created_at')
+
